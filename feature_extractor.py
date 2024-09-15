322f0dd18ce95e4a0aaf946220f198dc07584768
unknown <sakarim9124@gmail.com>
2024-09-16 01:55:52 +0530

    updated

--- feature_extractor.py
+++ feature_extractor.py
@@ -17,3 +17,45 @@
 #
 # # we will dump it to binary obj
 # pickle.dump(fileNames, open('filenames.pkl', 'wb'))
+
+
+#######################################
+#######################################
+
+from tensorflow.keras.preprocessing import image
+from keras_vggface.utils import preprocess_input
+from keras_vggface.vggface import VGGFace
+import numpy as np
+import pickle
+from tqdm import tqdm
+
+# Load filenames from the pickle file
+filenames = pickle.load(open('filenames.pkl', 'rb'))
+
+# Initialize the VGGFace model using ResNet50 backbone
+model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+
+
+# Print model summary to verify the architecture
+# model.summary()
+def feature_extractor(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    expanded_img = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img)
+
+    result = model.predict(preprocessed_img).flatten()
+
+    return result
+
+
+features = []
+
+for file in tqdm(filenames):
+    features.append(feature_extractor(file, model))
+    # result = feature_extractor(file, model)
+
+    # print(result.shape)
+    # break
+
+    pickle.dump(features, open('embedding.pkl', 'wb'))
